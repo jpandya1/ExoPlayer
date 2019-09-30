0a1e5d816cfffe985c62ff985e800c308ca0556f
Janvi Pandya <janvipandya@utexas.edu>
2019-09-29 19:46:54 -0500

    Got gradle to work

---
+++ run-tests.sh
@@ -0,0 +1,9 @@
+# This file runs the projects tests, and outputs them into test-execution.txt
+# We need to build the build.gradle using gradle (not working)
+# cd into the testutil folder
+# run the test(s) using java 
+# output those results into a execution test
+# Also in a README.txt put the number of passed or failed tests
+
+cd ../ExoPlayer/testutils/src/test/java/com/google/android/exoplayer2/testutil
+
